2aafeb36cb064f5c7a4e6d0dd1eb60fd929b19be
Dmytro Mamenko <mamenko.dv@gmail.com>
2021-03-19 23:38:05 +0100

    feat: add highlite to letters and note buttons

--- virtual-piano/virtual-piano.js
+++ virtual-piano/virtual-piano.js
@@ -1,7 +1,6 @@
 "use strict";
-
+//fullscreen API  functionality
 const fullScreenButton = document.querySelector(".fullscreen");
-console.log(fullScreenButton);
 
 function toggleFullScreen() {
   if (!document.fullscreenElement) {
@@ -12,5 +11,35 @@ function toggleFullScreen() {
     }
   }
 }
-
 fullScreenButton.addEventListener("click", () => toggleFullScreen());
+
+//piano functionality
+// piano.addEventListener("click", (event) => {
+//   if (event.target.classList.contains("piano-key")) {
+//     pianoКeys.forEach((el) => {
+//       if (el.classList.contains("active")) {
+//         el.classList.remove("active");
+//       }
+//     });
+//     event.target.classList.add("active");
+//   }
+// });
+
+// show and hide letters and notes
+const lettersButton = document.querySelector(".btn-letters");
+console.log(lettersButton.tagName);
+const notesButton = document.querySelector(".btn-notes");
+const signСontrolButtons = document.querySelector(".btn-container");
+signСontrolButtons.addEventListener("click", (e) => {
+  if (e.target.tagName != "BUTTON") return;
+  toggleVisibility(e.target);
+});
+let activeElement;
+function toggleVisibility(element) {
+  element.classList.toggle("btn-active");
+  if (activeElement) {
+    activeElement.classList.remove("btn-active");
+  }
+  activeElement = element;
+  activeElement.classList.add("btn-active"); //
+}
