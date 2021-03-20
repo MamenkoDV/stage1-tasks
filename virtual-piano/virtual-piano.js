bf6c1f8551e206b2b120f4282d19f0b1ab1beed8
Dmytro Mamenko <mamenko.dv@gmail.com>
2021-03-20 01:06:37 +0100

    feat: notes and letters complete realization

--- virtual-piano/virtual-piano.js
+++ virtual-piano/virtual-piano.js
@@ -27,19 +27,32 @@ fullScreenButton.addEventListener("click", () => toggleFullScreen());
 
 // show and hide letters and notes
 const lettersButton = document.querySelector(".btn-letters");
-console.log(lettersButton.tagName);
 const notesButton = document.querySelector(".btn-notes");
 const signСontrolButtons = document.querySelector(".btn-container");
+const pianoKeys = Array.from(document.querySelectorAll(".piano-key"));
+const piano = document.querySelector(".piano");
+
 signСontrolButtons.addEventListener("click", (e) => {
   if (e.target.tagName != "BUTTON") return;
-  toggleVisibility(e.target);
+  toggleVisibility(e.target, "btn-active");
+  e.target === lettersButton
+    ? pianoKeys.forEach((element) => element.classList.add("piano-key-letter"))
+    : pianoKeys.forEach((element) =>
+        element.classList.remove("piano-key-letter")
+      );
 });
+//   pianoKeys.forEach((element) =>
+//     console.log(element.getAttribute("data-letter"))
+//   );
+//   pianoKeys.forEach((element) =>
+//     console.log(element.getAttribute("data-notes"))
+//   );
 let activeElement = notesButton;
-function toggleVisibility(element) {
-  element.classList.toggle("btn-active");
+function toggleVisibility(element, classForToggle) {
   if (activeElement) {
-    activeElement.classList.remove("btn-active");
+    activeElement.classList.remove(classForToggle);
   }
   activeElement = element;
-  activeElement.classList.add("btn-active");
+  activeElement.classList.add(classForToggle);
 }
+function signKeys(element, classForToggle) {}
